14c42ad02abe2617214c6ccaf24431f63917e654
Tmx1997 <52399563+Tmx1997@users.noreply.github.com>
2019-08-07 19:26:42 +0800

    Update saa.java

--- src/main/java/com/kgc/hfr/controller/saa.java
+++ src/main/java/com/kgc/hfr/controller/saa.java
@@ -13,7 +13,7 @@ import org.springframework.stereotype.Controller;
  */
 @Controller
 public class saa {
-  public static void mian(String[]args){
+  public static void main(String[]args){
   System.out.println(333);
   }
 }
